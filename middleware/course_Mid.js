bf515aed01729b411a7a860298effcdab548cbf5
adgshs99 <anasdgsh@hotmail.com>
2025-04-09 20:16:24 +0300

    update

--- middleware/course_Mid.js
+++ middleware/course_Mid.js
@@ -1,3 +1,34 @@
-function AddCourse(req,res,next){
+async function AddCourse(req,res,next){
+    let name = addSlashes(req.body.name);// a' or (drop table courses --
+    let Query=`INSERT INTO courses( name) VALUES ('${name}')`;
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
+    }
 
+    next();
+}
+async function GetAllCourses(req,res,next){
+    let Query="SELECT * FROM courses";
+    const promisePool = db_pool.promise();
+    let rows=[];
+    req.courses_data=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.courses_data=rows;
+    } catch (err) {
+        console.log(err);
+    }
+
+    next();
+}
+async function DeleteCourse(req,res,next){
+
+}
+module.exports = {
+    AddCourse,
+    GetAllCourses,
 }
